7772b3293378d711c33649946f47a7b265a90cdf
albutko <alex.butenko@altirnao.com>
2019-07-29 11:15:51 -0400

    Initial commit with simple-gauge

---
+++ simple-gauge/src/index.js
@@ -0,0 +1,79 @@
+const dscc = require('@google/dscc');
+const d3 = require('d3');
+const viz = require('@google/dscc-scripts/viz/initialViz.js');
+const local = require('./localMessage.js');
+
+// change this to 'true' for local development
+// change this to 'false' before deploying
+export const LOCAL = false;
+
+// write viz code here
+const drawViz = (data) => {
+
+  const height = (dscc.getHeight()-4);
+  const width = (dscc.getWidth()- 4);
+
+  d3.select("body").selectAll('svg').remove();
+
+  const svg = d3.select('body').append("svg")
+       .attr("viewBox", [-width / 2 , -height / 2, width, height]);
+
+   let val = +data.tables.DEFAULT[0].metric;
+   const min = +data.style.arcMin.value ? +data.style.arcMin.value : +data.style.arcMin.defaultValue
+   const max = +data.style.arcMax.value ? +data.style.arcMax.value : +data.style.arcMax.defaultValue;
+   const secondLevel = +data.style.secondLevel.value ? +data.style.secondLevel.value : +data.style.secondLevel.defaultValue;
+   const thirdLevel = +data.style.thirdLevel.value ? +data.style.thirdLevel.value : +data.style.thirdLevel.defaultValue;
+   const fullArcDegrees = +data.style.arcLength.value ? +data.style.arcLength.value : +data.style.arcLength.defaultValue;
+   var outerRad = (Math.min(width, height) - 20)/2
+   var innerRad = outerRad*3/4;
+   const smallCircleRad = innerRad*1/10;
+   const rectWidthRatio = 1
+
+
+   var arcRands = fullArcDegrees*Math.PI/(2*180)
+   let arcScale = d3.scaleLinear().domain([min, max]).range([-arcRands, arcRands]);
+
+   let arcData = [
+     {"class": "lower", "startAngle": min, "endAngle": secondLevel, "color": "red"},
+     {"class": "middle", "startAngle": secondLevel, "endAngle": thirdLevel, "color": "yellow"},
+     {"class": "upper", "startAngle": thirdLevel, "endAngle": max, "color": "green"}
+   ]
+
+   var arc = d3.arc()
+       .innerRadius(innerRad)
+       .outerRadius(outerRad)
+       .startAngle(function(d){return arcScale(d.startAngle);})
+       .endAngle(function(d){return arcScale(d.endAngle);})
+       .cornerRadius(2);
+
+   var valRads = arcScale(Math.max(Math.min(val,max)));
+
+   svg.append("g").selectAll("path")
+     .data(arcData)
+     .enter()
+     .append("path")
+     .attr("d", arc).attr("fill", function(d){return d.color;}).attr("stroke", "white");
+
+
+
+
+   svg.append("circle")
+   .attr("r", smallCircleRad).attr("fill", "black").attr("cx", 0).attr("cy", 0);
+
+
+   svg
+     .append("rect")
+     .attr("width", smallCircleRad*rectWidthRatio)
+     .attr("height", (innerRad - 10))
+     .attr("fill","black")
+     .attr("transform", "translate(" + -(smallCircleRad*rectWidthRatio)/2 + " 0)" +
+           "rotate(" + (180 +  180*valRads/Math.PI) + " " +  (smallCircleRad*rectWidthRatio)/2 + " 0)" );
+}
+
+
+// renders locally
+if (LOCAL) {
+  drawViz(local.message);
+} else {
+  dscc.subscribeToData(drawViz, {transform: dscc.objectTransform});
+}
---
+++ simple-gauge/src/localMessage.js
@@ -0,0 +1,164 @@
+/**
+ * This file provides the mock "data" received
+ * by your visualization code when you develop
+ * locally.
+ *
+ */
+ export const message = {
+   "tables": {
+     "DEFAULT": [
+       {
+         "metric": [
+           "150"
+         ]
+       }
+     ]
+   },
+   "fields": {
+     "metric": [
+       {
+         "id": "qt_b1rdq1wizb",
+         "name": "Record Count",
+         "type": "NUMBER",
+         "concept": "METRIC"
+       }
+     ]
+   },
+   "style": {
+     "arcLength": {
+       "value": 270,
+       "defaultValue": 180
+     },
+     "arcMin": {
+       "value": 0,
+       "defaultValue": 0
+     },
+     "arcMax": {
+       "value": 99,
+       "defaultValue": 99
+     },
+     "secondLevel": {
+       "value": 33,
+       "defaultValue": 33
+     },
+     "thirdLevel": {
+       "value": 66,
+       "defaultValue": 66
+     }
+   },
+   "theme": {
+     "themeFillColor": {
+       "color": "#fff",
+       "opacity": 1
+     },
+     "themeFontColor": {
+       "color": "#616161",
+       "opacity": 1
+     },
+     "themeFontFamily": "Roboto",
+     "themeAccentFillColor": {
+       "color": "#4285F4",
+       "opacity": 1
+     },
+     "themeAccentFontColor": {
+       "color": "#EFEFEF",
+       "opacity": 1
+     },
+     "themeAccentFontFamily": "Roboto",
+     "themeSeriesColor": [
+       {
+         "color": "#4285F4",
+         "opacity": 1
+       },
+       {
+         "color": "#DB4437",
+         "opacity": 1
+       },
+       {
+         "color": "#F4B400",
+         "opacity": 1
+       },
+       {
+         "color": "#0F9D58",
+         "opacity": 1
+       },
+       {
+         "color": "#AB47BC",
+         "opacity": 1
+       },
+       {
+         "color": "#00ACC1",
+         "opacity": 1
+       },
+       {
+         "color": "#FF7043",
+         "opacity": 1
+       },
+       {
+         "color": "#9E9D24",
+         "opacity": 1
+       },
+       {
+         "color": "#5C6BC0",
+         "opacity": 1
+       },
+       {
+         "color": "#F06292",
+         "opacity": 1
+       },
+       {
+         "color": "#00796b",
+         "opacity": 1
+       },
+       {
+         "color": "#c2185b",
+         "opacity": 1
+       },
+       {
+         "color": "#7e57c2",
+         "opacity": 1
+       },
+       {
+         "color": "#03a9f4",
+         "opacity": 1
+       },
+       {
+         "color": "#8bc34a",
+         "opacity": 1
+       },
+       {
+         "color": "#fdd835",
+         "opacity": 1
+       },
+       {
+         "color": "#fb8c00",
+         "opacity": 1
+       },
+       {
+         "color": "#8d6e63",
+         "opacity": 1
+       },
+       {
+         "color": "#9e9e9e",
+         "opacity": 1
+       },
+       {
+         "color": "#607d8b",
+         "opacity": 1
+       }
+     ],
+     "themeIncreaseColor": {
+       "color": "#388e3c",
+       "opacity": 1
+     },
+     "themeDecreaseColor": {
+       "color": "#f44336",
+       "opacity": 1
+     },
+     "themeGridColor": {
+       "color": "#e9e9e9",
+       "opacity": 1
+     }
+   },
+   "interactions": {}
+ };
